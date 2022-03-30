9fa2e6d5cbf9529c42b6184459a3a1b1d990160f
12323 <http://git>
2022-03-30 09:20:50 +0800

    spring3.0

--- ch02-di-xml_/src/test/java/myTest.java
+++ ch02-di-xml_/src/test/java/myTest.java
@@ -1,7 +1,13 @@
+import com.bjpowernode.ba01.School;
 import com.bjpowernode.ba01.Student;
 import org.junit.Test;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@ComponentScan(value = "com.bjpowernode.ba01")
 public class myTest {
     @Test
     public void test2(){
@@ -9,6 +15,7 @@ public class myTest {
         Student student = (Student) applicationContext.getBean("MyStudent");
 //        从容器中获取Student
         System.out.println(student);
+
 
 
     }
